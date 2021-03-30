135daee1b0fa2700d333b9983047713a11f854a2
Guilherme Senna <guisenna23@hotmail.com>
2021-03-30 10:54:51 -0300

    Starting application with some Data

--- src/main/java/com/mylearning/todolist/repo/TodoItems.java
+++ src/main/java/com/mylearning/todolist/repo/TodoItems.java
@@ -2,7 +2,9 @@ package com.mylearning.todolist.repo;
 
 import com.mylearning.todolist.model.TodoItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoItems extends JpaRepository<TodoItem, Long> {
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TODO_ITEM (id, title, description,done) VALUES
+(1, 'Task 1', 'Math exercise',true),
+(2, 'Task 2', 'Portuguese exercise',false);
